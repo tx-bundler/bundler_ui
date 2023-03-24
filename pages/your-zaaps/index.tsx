61ef4cd55357090374ab7f13395fe08a357543c0
berkingurcan <berkingurcan@yandex.com>
2023-03-24 14:51:24 +0700

    Update index.tsx

--- pages/your-zaaps/index.tsx
+++ pages/your-zaaps/index.tsx
@@ -37,7 +37,7 @@ import { testAbi }  from "../../constants/abis/testAbi";
 import { LendingAbi } from "@/constants/abis/LendingAbi";
 
 
-export default function Swap({walletAddress}: any) {
+export default function Swap() {
   const router = useRouter();
   const [isOpen, setIsOpen] = useState(false);
   const { address, isConnected, isDisconnected } = useAccount();
@@ -47,8 +47,8 @@ export default function Swap({walletAddress}: any) {
   const WETH_ADDRESS = "0x20b28B1e4665FFf290650586ad76E977EAb90c5D";
   const DAI_ADDRESS = "0x3e7676937A7E96CFB7616f255b9AD9FF47363D4b";
   const DAI_DECIMALS = 18;
-  const POOL_ADDRESS = "0xe52940eDDa6ec5FDabef7C33B9C1E1d613BbA144"; // ETH/DAI
   const VAULT_CONTRACT_ADDRESS = "0x4Ff94F499E1E69D687f3C3cE2CE93E717a0769F8";
+  const POOL_ADDRESS = "0xe52940eDDa6ec5FDabef7C33B9C1E1d613BbA144"; // ETH/DAI
   const ROUTER_ADDRESS = "0xB3b7fCbb8Db37bC6f572634299A58f51622A847e";
   const POOLFACTORY_ADDRESS = "0xf2FD2bc2fBC12842aAb6FbB8b1159a6a83E72006"; // Classic
   const ADDRESS_ZERO = ethers.constants.AddressZero;
@@ -212,15 +212,11 @@ export default function Swap({walletAddress}: any) {
                   <Button onClick={() => write2?.()}> BORROW </Button>
                   <Button onClick={handleSwap}> SWAP </Button>
 
+                  {isLoading1 && <div>Check Wallet</div>}
+                  {isSuccess1 && <div>Transaction: {JSON.stringify(data1)}</div>}
+                  {isSuccess2 && <div>Transaction: {JSON.stringify(data2)}</div>}
+                  {isSuccess3 && <div>Transaction: {JSON.stringify(data3)}</div>}
 
-        
-      {isLoading1 && <div>Check Wallet</div>}
-      {isSuccess1 && <div>Transaction: {JSON.stringify(data1)}</div>}
-      {isSuccess2 && <div>Transaction: {JSON.stringify(data2)}</div>}
-      {isSuccess3 && <div>Transaction: {JSON.stringify(data3)}</div>}
-
-            
-                           
                   <SwapModal isOpen={isOpen} onClose={() => setIsOpen(false)} /> 
 
                 </Text>
